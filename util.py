767bc0a64b85c45847bbb3fc28e532988a4bde8b
Lucy He <luxihe@college.harvard.edu>
2022-02-05 00:37:56 -0500

    added MAP and MLL loss

--- util.py
+++ util.py
@@ -71,4 +71,16 @@ def to_np(v):
         return v.detach().cpu().numpy()
     return v.detach().numpy()
 
+def zero_mean_unit_var_normalization(X, mean=None, std=None):
+    if mean is None:
+        mean = np.mean(X, axis=0)
+    if std is None:
+        std = np.std(X, axis=0)
 
+    X_normalized = (X - mean) / std
+
+    return X_normalized, mean, std
+
+
+def zero_mean_unit_var_denormalization(X_normalized, mean, std):
+    return X_normalized * std + mean
